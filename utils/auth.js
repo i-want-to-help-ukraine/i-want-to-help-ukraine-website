eaf0c3c17688efde4b37c8fcbfde701fea8b96b9
Olena Domchevska <alena181823@gmail.com>
2022-03-27 23:46:12 +0200

    feat(4): update authentication

--- utils/auth.js
+++ utils/auth.js
@@ -5,6 +5,7 @@ export const PROTECTED_ROUTES = ['edit-profile']
 const auth0 = new Auth0Client({
   domain: process.env.AUTH0_DOMAIN,
   client_id: process.env.AUTH0_CLIENT_ID,
+  audience: process.env.AUTH0_API_URL,
   useRefreshTokens: true,
 })
 
