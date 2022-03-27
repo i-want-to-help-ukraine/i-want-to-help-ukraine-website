b3e64cea3b1971fb1c42e285f43764e0a8b18954
Olena Domchevska <alena181823@gmail.com>
2022-03-27 21:29:50 +0200

    feat(4): remove extra authorize

--- pages/auth-callback.vue
+++ pages/auth-callback.vue
@@ -5,31 +5,14 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import { CustomLoader } from '../components/UI/index.js'
 
 export default {
-  // The responsibility of the page is to fetch and
-  // save to store user profile.
   name: 'AuthCallback',
   components: { CustomLoader },
   layout: 'empty',
-
-  computed: mapState({
-    token: ({ auth }) => auth.token,
-  }),
   mounted() {
-    this.authorize()
-  },
-  methods: {
-    async authorize() {
-      try {
-        await this.$store.dispatch('auth/authorize')
-        this.$router.push('/edit-profile')
-      } catch {
-        this.$router.push('/')
-      }
-    },
+    this.$router.push('/edit-profile')
   },
 }
 </script>
