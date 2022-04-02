c95ee68fc9a4e25753a2697cb21cd61064c29f07
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-04-02 08:44:29 +0200

    fix: fix app url env

--- store/auth.js
+++ store/auth.js
@@ -122,9 +122,9 @@ export const actions = {
   },
   login() {
     try {
-      console.log('login', process.env.APP_URL)
+      console.log('login')
       auth0.loginWithRedirect({
-        redirect_uri: `${process.env.APP_URL}/auth-callback`,
+        redirect_uri: `https://volunteers-ua.com/auth-callback`,
       })
     } catch (error) {
       console.error(`Login Error: ${error.message}`)
