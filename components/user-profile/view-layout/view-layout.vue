5dd90fd6cfeffa73b90d67544bc223b1e5433d63
Daniel Kazanskiy <slonanezametil@gmail.com>
2022-04-05 14:15:38 +0200

    feat(3): user info width fixed

--- components/user-profile/view-layout/view-layout.vue
+++ components/user-profile/view-layout/view-layout.vue
@@ -3,7 +3,7 @@
     v-if="userInfo"
     class="flex flex-col md:flex-row justify-between my-8 px-4 w-full"
   >
-    <div class="w-2/3">
+    <div class="md:w-2/3">
       <main-info :user-info="userInfo" class="mb-8" />
       <activity-info
         v-if="userInfo.activities && userInfo.activities.length > 0"
@@ -11,7 +11,6 @@
       />
     </div>
     <div class="mt-8 md:mt-0 md:ml-8">
-      <!-- <contact-info :contacts="userInfo.contacts" class="mb-4" /> -->
       <social-info
         v-if="userInfo.social && userInfo.social.length > 0"
         :user-info="userInfo.social"
@@ -27,7 +26,6 @@ import SocialInfo from './social-info.vue'
 import ActivityInfo from './activity-info.vue'
 import MainInfo from './main-info.vue'
 import PaymentInfo from './payment-info.vue'
-// import ContactInfo from './contact-info.vue'
 
 export default {
   name: 'ViewLayout',
@@ -36,7 +34,6 @@ export default {
     SocialInfo,
     ActivityInfo,
     PaymentInfo,
-    // ContactInfo,
   },
   props: {
     userInfo: {
