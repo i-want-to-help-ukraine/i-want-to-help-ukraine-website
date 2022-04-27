8c1bc734714aa4fee208b8565987e38c0ec469f8
Daniel Kazanskiy <slonanezametil@gmail.com>
2022-04-27 11:37:07 +0200

    change volunteer card image styles

--- components/index/volunteer-card.vue
+++ components/index/volunteer-card.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="h-min md:h-full p-3 flex flex-col justify-between rounded-xl bg-white hover:bg-grey duration-150"
+    class="h-min md:h-full flex flex-col justify-between overflow-hidden rounded-xl bg-white hover:bg-grey duration-150"
   >
-    <div>
-      <user-pic :src="volunteer.avatarUrl" class="mb-4 w-full rounded-xl" />
+    <user-pic :src="volunteer.avatarUrl" class="w-full rounded-lg" />
+    <div class="p-3">
       <h5 class="text-md sm:text-xl mr-2">
         {{ volunteer.firstName }} {{ volunteer.lastName }}
       </h5>
@@ -18,7 +18,7 @@
         }}
       </p>
     </div>
-    <p class="mt-6 mb-4 sm:mb-0 text-center sm:text-left">View More</p>
+    <p class="pt-3 p-6 sm:pt-0 sm:p-3 text-center sm:text-left">View More</p>
   </div>
 </template>
 
