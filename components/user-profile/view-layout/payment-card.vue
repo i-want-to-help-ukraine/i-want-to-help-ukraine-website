e45b8a989d44a8c089338a97c145fe71b6219804
Olena Domchevska <alena181823@gmail.com>
2022-04-28 23:34:19 +0200

    fix(000): payment card styles

--- components/user-profile/view-layout/payment-card.vue
+++ components/user-profile/view-layout/payment-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="text-lg uppercase">
+    <p class="text-lg">
       {{ payment.provider.title }}
     </p>
     <div
@@ -10,13 +10,14 @@
     >
       <span class="col-span-1" v-if="key !== 'value'">{{ key }}</span>
       <a
-        :href="value"
-        class="col-span-3 break-all"
         v-if="payment.provider.title === 'send.monobank.ua'"
+        target="_blank"
+        :href="value.includes('//') ? value : '//' + value"
+        class="col-span-3 break-all font-bold"
       >
         {{ value.length > 32 ? `${value.slice(0, 32)}...` : value }}
       </a>
-      <span @click="onCopy(value)" @keydown="onCopy" v-else>
+      <span v-else class="font-bold" @click="onCopy(value)" @keydown="onCopy">
         {{ value }}
       </span>
     </div>
